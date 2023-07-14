2626b51e15c173133ce787df473e09d88d8910f2
vincent <100250184+vguzman812@users.noreply.github.com>
2023-07-14 11:24:23 -0400

    Add template 1 and template 2

---
+++ template2/assets/js/main.js
@@ -0,0 +1,97 @@
+
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body')
+	
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 200);
+		});
+
+	// Gallery.
+	$window.on('load', function() {
+
+		var $gallery = $('.gallery');
+
+		$gallery.poptrox({
+			baseZIndex: 10001,
+			useBodyOverflow: false,
+			usePopupEasyClose: false,
+			overlayColor: '#1f2328',
+			overlayOpacity: 0.95,
+			usePopupDefaultStyling: true,
+			usePopupCaption: true,
+			popupLoaderText: '',
+			windowMargin: 50,
+			usePopupNav: true
+		});
+
+		// Hack: Adjust margins when 'small' activates.
+			breakpoints.on('>small', function() {
+				$gallery.each(function() {
+					$(this)[0]._poptrox.windowMargin = 50;
+				});
+			});
+
+			breakpoints.on('<=small', function() {
+				$gallery.each(function() {
+					$(this)[0]._poptrox.windowMargin = 5;
+				});
+			});
+
+	});
+
+	// Section transitions.
+	if (browser.canUse('transition')) {
+
+		var on = function() {
+
+
+			// Generic sections.
+				$('.wrapper')
+					.scrollex({
+						mode:		'middle',
+						delay:		10,
+						initialize:	function() { $(this).addClass('inactive'); },
+						terminate:	function() { $(this).removeClass('inactive'); },
+						enter:		function() { $(this).removeClass('inactive'); },
+						leave:		function() { $(this).addClass('inactive'); }
+					});
+
+
+		};
+
+		var off = function() {
+
+
+			// Generic sections.
+				$('.wrapper')
+					.unscrollex();
+
+			// Footer.
+				$('#footer')
+					.unscrollex();
+
+		};
+
+		breakpoints.on('<=small', off);
+		breakpoints.on('>small', on);
+
+	};
+
+})(jQuery);
